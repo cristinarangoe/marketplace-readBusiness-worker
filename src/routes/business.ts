9e26c31316177b800835a2c6aef5cf2307a6efea
M4rteaga <m4rteagag@gmail.com>
2022-11-13 18:07:08 -0500

    fix path params

--- src/routes/business.ts
+++ src/routes/business.ts
@@ -2,30 +2,29 @@ import { BusinessDB } from './../types';
 import { Hono } from 'hono';
 import * as Realm from 'realm-web';
 
-
 const business = new Hono();
 
 let RealmApp: Realm.App;
 const ObjectId = Realm.BSON.ObjectID;
 
-business.get('/findBusiness', async (c) => {
+business.get('/findBusiness/:email', async (c) => {
 	try {
+		const email = c.req.param('email');
 		RealmApp = RealmApp || new Realm.App(c.env.MONGO_DB_APP_ID);
 
 		const credentials = Realm.Credentials.apiKey(c.env.MONGO_DB_API_KEY);
 
-		//duda 
+		//duda
 		let user = await RealmApp.logIn(credentials);
 		let mongoClient = user.mongoClient('mongodb-atlas');
 
 		const collection = mongoClient
 			.db('users')
 			.collection<BusinessDB>('entrepreneur');
-        //falta poner que busque es por el negocio, no por el name
-		const result = await collection.findOne({email:'mateoarteagagiraldo@gmail.com'});
+		//falta poner que busque es por el negocio, no por el name
+		const result = await collection.findOne({ email: email });
 
-
-    return c.json(result)
+		return c.json(result);
 	} catch (error) {
 		console.log(error);
 		return new Response(JSON.stringify((error as Error).message), {
